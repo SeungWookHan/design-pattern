9ee029a9041ed481a0da7ce0d323a966bc539f6f
SeungWookHan <hswook12@me.com>
2022-02-22 09:54:37 +0900

    Change printer to HashTagPrinter

--- 07_adapter/main.js
+++ 07_adapter/main.js
@@ -1,9 +1,15 @@
-import Printer from './printer.js'
+// import Printer from './printer.js'
+import HashTagPrinter from "./hashTagPrinter.js"
 
-let printer = new Printer();
+// let printer = new Printer();
+// printer.pushText("Hello");
+// printer.pushText("Test");
+// printer.pushText("Pattern");
+
+let printer = new HashTagPrinter(); // HashTagPrinter의 printer로 교체
 printer.pushText("Hello");
 printer.pushText("Test");
 printer.pushText("Pattern");
 
-let result = printer.print();
+let result = printer.print(); // printer가 명세가 맞지 않아 에러가 발생
 console.log(result)
