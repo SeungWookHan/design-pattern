07573cc77cf7ffd26b28cd02f691e2408fbeace8
SeungWookHan <hswook12@me.com>
2022-02-22 09:50:40 +0900

    Add hashTagPrinter

---
+++ 07_adapter/hashTagPrinter.js
@@ -0,0 +1,13 @@
+class HashTagPrinter {
+  constructor() {
+    this.textArr = [];
+  }
+
+  pushText(text) {
+    this.textArr.push(text);
+  }
+
+  printWithHashTag() {
+    return this.textArr.map(text => `#${text}`).join(' ');
+  }
+}
